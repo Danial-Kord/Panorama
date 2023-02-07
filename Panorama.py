810d2a34086a9cf4789d81aae986e9b444cad083
Danial <danial.kordmodanlou@gmail.com>
2023-02-07 14:17:17 +0330

    [feat]: saving output

--- Panorama.py
+++ Panorama.py
@@ -8,7 +8,7 @@ def RANSAC(src_pts, dst_pts, num_iterations, threshold):
     best_inliers = []
     for i in range(num_iterations):
         # Randomly select 4 pairs of matched points
-        random_indices = np.random.randint(0, len(src_pts), 8)
+        random_indices = np.random.randint(0, len(src_pts), 4)
         random_src_pts = src_pts[random_indices]
         random_dst_pts = dst_pts[random_indices]
 
@@ -35,8 +35,8 @@ def RANSAC(src_pts, dst_pts, num_iterations, threshold):
 
 
 # Load the two images
-img1 = cv2.imread("1.png")
-img2 = cv2.imread("2.png")
+img1 = cv2.imread("2.png")
+img2 = cv2.imread("1.png")
 
 # Initialize SIFT detector
 sift = cv2.xfeatures2d.SIFT_create()
@@ -67,6 +67,34 @@ src_pts = np.float32([kp1[m.queryIdx].pt for m in good]).reshape(-1, 1, 2)
 dst_pts = np.float32([kp2[m.trainIdx].pt for m in good]).reshape(-1, 1, 2)
 
 # Homography and RANSAC
-homography_matrix, inliers = RANSAC(src_pts, dst_pts, 1000, 2500)
+homography_matrix, inliers = RANSAC(src_pts, dst_pts, 100, 2500)
+#
+#
 
+#
+# # save Inliers
+inlier_matches = [good[i] for i in inliers]
+img_inliers = cv2.drawMatches(img1, kp1, img2, kp2, inlier_matches, None, flags = cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS, matchColor = (0,255,0))
+cv2.imwrite("2_inliers.png", img_inliers)
 
+# save Outliers
+outlier_matches = [good[i] for i in range(len(good)) if i not in inliers]
+img_outliers = cv2.drawMatches(img1, kp1, img2, kp2, outlier_matches, None, flags = cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS, matchColor = (255,0,0))
+cv2.imwrite("2_outliers.png", img_outliers)
+
+homography_matrix, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 10000.0)
+
+result = cv2.warpPerspective(img2, homography_matrix, (img1.shape[1] + img2.shape[1], img1.shape[0]))
+
+# Create a separate image for the destination
+dst = np.zeros((img1.shape[0], img1.shape[1] + img2.shape[1], 3), dtype=np.uint8)
+
+# Copy the first image to the destination image
+dst[0:img1.shape[0], 0:img1.shape[1]] = img1
+
+# Copy the transformed second image to the destination image
+dst[0:img1.shape[0], img1.shape[1]:img1.shape[1]+img2.shape[1]] = result[0:img1.shape[0], 0:img2.shape[1]]
+# Save the result
+cv2.imwrite("panorama.png", dst)
+
+# exit(0)
